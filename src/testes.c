12d673bcecf8cca49b26d5556405939f629428b1
Carlos Roberto <carlos.dukep@gmail.com>
2013-07-09 00:13:57 -0300

    Definiccoes do header.

--- src/testes.c
+++ src/testes.c
@@ -1,6 +1,6 @@
 //Irei iniciar relembrando a base de c, entao esse arquivo tera algunas estruturas ponteiros e variaveis que nao serao usadas no sistema.
 #include <stdlib.h> //Biblioteca com varias funcoes usadas durante a execucao do programa.
-#include <stdio.h> //Para tratamento de arquivo e funcoes para exibicao de mensagens na tela.
+#include <stdio.h> //Para controle de entrada e saida de stream de dados.
 
 #include "include/teste.h"
 
